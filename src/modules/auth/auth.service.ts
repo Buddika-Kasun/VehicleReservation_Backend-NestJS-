bbf020540b18083c5c35dfc8cae36e8602683fae
Buddika Kasun <buddikakasun80@gmail.com>
2025-10-31 18:23:42 +0530

    Add auth service

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -7,19 +7,34 @@ import { compare } from '../../common/utils/hash.util';
 export class AuthService {
   constructor(private usersService: UsersService, private jwtService: JwtService) {}
 
-  async validateUser(email: string, pass: string): Promise<any> {
-    const user = await this.usersService.findByEmail(email);
-    if (user && (await compare(pass, user.password))) {
+  async validateUser(username: string, pass: string): Promise<any> {
+    const user = await this.usersService.findByUsername(username);
+
+    if (!user.isApproved) throw new UnauthorizedException('Account pending HR approval.');
+
+    if (user && (await compare(pass, user.passwordHash))) {
       const { password, ...result } = user as any;
       return result;
     }
+    
     throw new UnauthorizedException('Invalid credentials');
+
   }
 
   async login(user: any) {
-    const payload = { sub: user.id, email: user.email, role: user.role };
+    const payload = { sub: user.id, email: user.email, role: user.role, username: user.username };
+
+    const token = this.jwtService.sign(payload)
+    
     return {
-      access_token: this.jwtService.sign(payload),
+      access_token: token,
+      user: {
+        id: user.id,
+        username: user.username,
+        displayname: user.display,
+        email: user.email,
+        role: user.role,
+      },
     };
   }
 }

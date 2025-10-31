1e379bd204fba35f173d6ecdf7a4fc9bcbba06f2
Buddika Kasun <buddikakasun80@gmail.com>
2025-10-31 18:23:55 +0530

    Add auth module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -6,14 +6,22 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { JwtStrategy } from './strategies/jwt.strategy';
 import { LocalStrategy } from './strategies/local.strategy';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     UsersModule,
     PassportModule,
-    JwtModule.register({
+    /*JwtModule.register({
       secret: process.env.JWT_SECRET || 'supersecretjwtkey',
       signOptions: { expiresIn: process.env.JWT_EXPIRES_IN || '1d' },
+    }),*/
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('jwt.secret'),
+        signOptions: { expiresIn: config.get<string>('jwt.expiresIn') },
+      }),
     }),
   ],
   providers: [AuthService, JwtStrategy, LocalStrategy],

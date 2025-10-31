07fd143249b4c8ba0ef99c7ca2739c84e7bf8348
Buddika Kasun <buddikakasun80@gmail.com>
2025-10-31 18:25:57 +0530

    Add modules and controllers

---
+++ src/modules/odometer/odometer.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('odometer')
+export class OdometerController {}
---
+++ src/modules/odometer/odometer.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { OdometerController } from './odometer.controller';
+import { OdometerService } from './odometer.service';
+
+@Module({
+  controllers: [OdometerController],
+  providers: [OdometerService]
+})
+export class OdometerModule {}

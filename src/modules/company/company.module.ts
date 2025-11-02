cf5f0003cb2ccf6c694901ca7f6305c4882a10bf
Buddika Kasun <buddikakasun80@gmail.com>
2025-11-02 22:21:47 +0530

    Add company module

--- src/modules/company/company.module.ts
+++ src/modules/company/company.module.ts
@@ -1,9 +1,16 @@
+
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { CompanyController } from './company.controller';
 import { CompanyService } from './company.service';
+import { Company } from '../../database/entities/company.entity';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([Company])
+  ],
   controllers: [CompanyController],
-  providers: [CompanyService]
+  providers: [CompanyService],
+  exports: [CompanyService]
 })
 export class CompanyModule {}

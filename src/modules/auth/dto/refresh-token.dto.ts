eaab4b2a67ee938f5c0867022e050979d1572deb
Buddika Kasun <buddikakasun80@gmail.com>
2025-11-02 10:59:21 +0530

    Update refresh token dto with swagger documentation

--- src/modules/auth/dto/refresh-token.dto.ts
+++ src/modules/auth/dto/refresh-token.dto.ts
@@ -1,6 +1,14 @@
-import { IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { IsJWT, IsNotEmpty } from 'class-validator';
 
 export class RefreshTokenDto {
+
+  @ApiProperty({
+    description: 'Refresh token to get new access token',
+    example: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'
+  })
   @IsNotEmpty()
+  @IsJWT()
   refreshToken: string;
+
 }

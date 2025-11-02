99dbf708cfad61d4749ea659ae88a9d82922fa51
Buddika Kasun <buddikakasun80@gmail.com>
2025-11-02 10:54:58 +0530

    Update user decorator

--- src/common/decorators/user.decorator.ts
+++ src/common/decorators/user.decorator.ts
@@ -1,8 +1,26 @@
+/*
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
-export const User = createParamDecorator(
+export const GetUser = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
     return request.user;
+  },
+);
+*/
+
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+import { User } from 'src/database/entities/user.entity';
+
+export const GetUser = createParamDecorator(
+  (data: keyof User | undefined, ctx: ExecutionContext): User | any => {
+    const request = ctx.switchToHttp().getRequest();
+    const user = request.user;
+    
+    if (data && user) {
+      return user[data]; // Return specific property if requested
+    }
+    
+    return user; // Return full user object
   },
 );

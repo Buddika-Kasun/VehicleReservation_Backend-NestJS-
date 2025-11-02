06c01a318f063dbc9b796792a784e31400a82ad3
Buddika Kasun <buddikakasun80@gmail.com>
2025-11-02 10:53:03 +0530

    Update jwt config file

--- src/config/jwt.config.ts
+++ src/config/jwt.config.ts
@@ -1,7 +1,9 @@
 
 export default () => ({
   jwt: {
-    secret: process.env.JWT_SECRET || 'supersecretjwtkey',
     expiresIn: process.env.JWT_EXPIRES_IN || '1d',
+    secret: process.env.JWT_SECRET || 'supersecretjwtkey',
+    refreshSecret: process.env.JWT_REFRESH_SECRET || 'supersecretrefreshkey',
+    refreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN || '7d',
   },
 });

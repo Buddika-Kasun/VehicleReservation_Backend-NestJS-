3bca9364e6401d42c6979b0cc5d5f887c7eced1d
Buddika Kasun <buddikakasun80@gmail.com>
2025-11-02 10:57:56 +0530

    Update login dto with swagger documentation

--- src/modules/auth/dto/login.dto.ts
+++ src/modules/auth/dto/login.dto.ts
@@ -1,9 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
 
 export class LoginDto {
+
+  @ApiProperty({
+    description: 'Unique username for the user',
+    example: 'john_doe'
+  })
   @IsNotEmpty()
   username: string;
 
+  @ApiProperty({
+    description: 'Password (min 6 characters)',
+    minLength: 6,
+    example: 'password123'
+  })
   @IsNotEmpty()
   password: string;
+  
 }

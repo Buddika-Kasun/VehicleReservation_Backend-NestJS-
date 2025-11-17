a8d325aaff5c4357f53d7ac48e5d8fefc4e3514c
Buddika Kasun <buddikakasun80@gmail.com>
2025-11-17 19:31:37 +0530

    Update update user dto

--- src/modules/users/dto/update-user.dto.ts
+++ src/modules/users/dto/update-user.dto.ts
@@ -3,10 +3,6 @@ import { CreateUserDto } from './create-user.dto';
 import { ApiPropertyOptional } from '@nestjs/swagger';
 
 export class UpdateUserDto extends CreateUserDto {
-  @ApiPropertyOptional({ description: 'Whether user is approved' })
-  @IsBoolean()
-  @IsOptional()
-  isApproved?: boolean; 
 
   @ApiPropertyOptional({ description: 'Whether user is active' })
   @IsBoolean()

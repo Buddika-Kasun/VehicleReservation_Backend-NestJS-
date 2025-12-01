687d9153efcc209d7dd4468ce84188fae26425fc
Buddika Kasun <buddikakasun80@gmail.com>
2025-12-01 22:07:51 +0530

    .

--- api/index.js
+++
@@ -1,7 +0,0 @@
-// api/index.js - This is the entry point for Vercel
-const { bootstrap } = require('../dist/main');
-
-module.exports = async (req, res) => {
-  const app = await bootstrap();
-  return app(req, res);
-};

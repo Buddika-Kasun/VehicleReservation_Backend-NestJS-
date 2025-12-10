04a2202ca4d4568d1ac8373f0a5b9cf72ca63402
Buddika Kasun <buddikakasun80@gmail.com>
2025-12-10 19:58:20 +0530

    Update vehicle module

--- src/modules/vehicles/vehicles.module.ts
+++ src/modules/vehicles/vehicles.module.ts
@@ -7,8 +7,6 @@ import { Vehicle } from 'src/database/entities/vehicle.entity';
 import { VehicleController } from './vehicles.controller';
 import { VehicleService } from './vehicles.service';
 import { ResponseService } from 'src/common/services/response.service';
-import { QrController } from './qr.controller';
-import { QrService } from './qr.service';
 import { Company } from 'src/database/entities/company.entity';
 import { CostConfiguration } from 'src/database/entities/cost-configuration.entity';
 
@@ -16,8 +14,8 @@ import { CostConfiguration } from 'src/database/entities/cost-configuration.enti
   imports: [
     TypeOrmModule.forFeature([Vehicle, Trip, OdometerLog, User, Company, CostConfiguration]),
   ],
-  controllers: [VehicleController, QrController],
-  providers: [VehicleService, QrService, ResponseService],
-  exports: [VehicleService, QrService],
+  controllers: [VehicleController],
+  providers: [VehicleService, ResponseService],
+  exports: [VehicleService],
 })
 export class VehicleModule {}
